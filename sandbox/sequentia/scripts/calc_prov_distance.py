24fc4b3266df8be9d3ea3805c709b32b6d607dec
Zhuoyi Huang <joeyfuerimmer@gmail.com>
2018-02-09 17:27:38 -0600

    calc_prov_distance.py applied site filtering with ns > 3/0.01

--- sandbox/sequentia/scripts/calc_prov_distance.py
+++ sandbox/sequentia/scripts/calc_prov_distance.py
@@ -3,7 +3,8 @@ import sys
 import numpy as np
 
 
-def calc_pop_distance(pop1, pop2, af_list_name, maf_min=0.001, nfile_max=-1):
+def calc_pop_distance(pop1, pop2, af_list_name, maf_min=0.01, nfile_max=-1,
+                      ac_min=3):
 
     af1_list = []
     af2_list = []
@@ -18,25 +19,35 @@ def calc_pop_distance(pop1, pop2, af_list_name, maf_min=0.001, nfile_max=-1):
             varkey, vardata = line.rstrip().split('\t')
             vardata = json.loads(vardata)
 
+            # drop MAF < maf_min
             if (vardata['All']['af_mle'] < maf_min or
                 1-vardata['All']['af_mle'] < maf_min):
                 continue
 
-            af1 = vardata[pop1]['af_mle']
-            af2 = vardata[pop2]['af_mle']
-            af1_list.append(af1 if af1 >= 0 else 0)
-            af2_list.append(af2 if af2 >= 0 else 0)
+            ns1 = vardata[pop1]['ns']
+            ns2 = vardata[pop2]['ns']
+
+            # drop sites with insufficient sample size
+            if ns1*maf_min < ac_min or ns2*maf_min < ac_min:
+                continue
+
+            af1 = max(vardata[pop1]['af_mle'], 0)
+            af2 = max(vardata[pop2]['af_mle'], 0)
+            af1_list.append(af1)
+            af2_list.append(af2)
             nvar += 1
 
         if nfile == nfile_max:
             break
 
-    cov_matrix = np.cov(af1_list, af2_list)
-
-    print(pop1, pop2, nvar,
-          cov_matrix[0][0], cov_matrix[1][1], cov_matrix[0][1],
-          cov_matrix[0][1]/np.sqrt(cov_matrix[0][0]*cov_matrix[1][1]),
-          sep='\t')
+    if nvar:
+        cov_matrix = np.cov(af1_list, af2_list)
+        print(pop1, pop2, nvar,
+              cov_matrix[0][0], cov_matrix[1][1], cov_matrix[0][1],
+              cov_matrix[0][1]/np.sqrt(cov_matrix[0][0]*cov_matrix[1][1]),
+              sep='\t')
+    else:
+        print(pop1, pop2, nvar, 0, 0, 0, 0, sep='\t')
 
 
 pop1, pop2, af_list_name, maf_min, nfile_max = sys.argv[1:6]
