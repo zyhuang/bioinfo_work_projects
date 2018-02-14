1235a4e8f3c4270fee43ba7d8cedccd5d0929c48
Zhuoyi Huang <joeyfuerimmer@gmail.com>
2018-02-14 10:24:41 -0600

    calc_pop_distance add euclidean distance

--- sandbox/sequentia/scripts/calc_pop_distance.py
+++ sandbox/sequentia/scripts/calc_pop_distance.py
@@ -3,7 +3,8 @@ import sys
 import numpy as np
 
 
-def calc_pop_distance(pop1, pop2, af_list_name, maf_min=0.001, nfile_max=-1):
+def calc_pop_angular_distance(pop1, pop2, af_list_name, maf_min=0.01,
+                              nfile_max=-1):
 
     af1_list = []
     af2_list = []
@@ -37,7 +38,33 @@ def calc_pop_distance(pop1, pop2, af_list_name, maf_min=0.001, nfile_max=-1):
           sep='\t')
 
 
+def calc_pop_euclidean_distance(pop1, pop2, af_list_name, maf_min=0.01,
+                                nfile_max=-1):
+
+    df_list = []
+    nfile = 0
+    for list_name in open(af_list_name):
+        list_name = list_name.rstrip()
+
+        nfile += 1
+        for line in open(list_name):
+            varkey, rsid, vardata = line.rstrip().split('\t')
+            vardata = json.loads(vardata)
+
+            if vardata['ALL'] < maf_min or 1-vardata['ALL'] < maf_min:
+                continue
+
+            df_list.append(vardata[pop1] - vardata[pop2])
+
+        if nfile == nfile_max:
+            break
+
+    nvar = len(df_list)
+    print(pop1, pop2, nvar, np.mean(df_list), np.std(df_list), sep='\t')
+
+
+
 pop1, pop2, af_list_name, maf_min, nfile_max = sys.argv[1:6]
 maf_min = float(maf_min)
 nfile_max = int(nfile_max)
-calc_pop_distance(pop1, pop2, af_list_name, maf_min, nfile_max)
+calc_pop_euclidean_distance(pop1, pop2, af_list_name, maf_min, nfile_max)
