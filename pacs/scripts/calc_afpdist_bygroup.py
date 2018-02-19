fced31b23a19d5a2812df3957e1d8656ac6fbb83
Zhuoyi Huang <joeyfuerimmer@gmail.com>
2018-02-18 23:58:32 -0600

    add calc_afpdist_bygroup.py to calculate per group pdist

--- pacs/scripts/calc_afpdist_bygroup.py
+++ pacs/scripts/calc_afpdist_bygroup.py
@@ -40,16 +40,63 @@ def calc_afpdist_byregion(binkey):
     fout.close()
 
 
-def calc_afpdist_bygene(binkey):
+def calc_afpdist_bygene(qgene):
 
+    gene_list = 'gene.binkey.list'
 
+    qtype = None
+    qbinkeys = []
+    print('reading ' + gene_list, file=sys.stderr)
+    for line in open(gene_list):
+        gene, gtype, binlist = line.rstrip().split('\t')
+        if qgene != gene:
+            continue
+        qtype = gtype
+        qbinkeys = binlist.split(',')
 
+    # qgene may contain '/'
+    out_gene_list = 'af_pdist_{}_list/{}.pdist.list'.format(qtype, qgene.replace('/','_'))
+
+    pdist_dict = {'pdist': {}}
+    for binkey in qbinkeys:
+        af_list = 'af_pdist_list/{}.pdist.list'.format(binkey)
+        print('reading ' + af_list, file=sys.stderr)
+        for line in open(af_list):
+            varkey, vardata = line.rstrip().split('\t')
+            vardata = json.loads(vardata)
+            for func in vardata['group']['func']:
+                gene, anno = func.split(':')
+                if gene != qgene:
+                    continue
+                if anno in ['upstream/downstream', 'intergenic']:
+                    continue
+
+                for pair in vardata['pdist']:
+                    if pair not in pdist_dict['pdist']:
+                        pdist_dict['pdist'][pair] = {
+                            'n': 0, 'df': 0, 'df2': 0,
+                        }
+                    for x in ['n', 'df', 'df2']:
+                        pdist_dict['pdist'][pair][x] \
+                            += vardata['pdist'][pair][x]
+
+                break   # each variant is counted once
+
+    print('writing ' + out_gene_list, file=sys.stderr)
+    fout = open(out_gene_list, 'w')
+    print(qgene, json.dumps(pdist_dict, sort_keys=True),
+          sep='\t', file=fout)
+    fout.close()
 
 
 
 
 if __name__ == '__main__':
 
-    group, binkey = sys.argv[1:3]
+    group = sys.argv[1]
     if group == 'region':
+        binkey = sys.argv[2]
         calc_afpdist_byregion(binkey)
+    elif group == 'gene':
+        qgene = sys.argv[2]
+        calc_afpdist_bygene(qgene)
