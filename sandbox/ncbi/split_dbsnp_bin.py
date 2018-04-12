92ce76b6be5de4dcc605c104080ccb7e9f0d2e33
Zhuoyi Huang <joeyfuerimmer@gmail.com>
2018-04-12 10:59:14 -0500

    split_dbsnp_bin.py add script to split variants in chromosome chunks into files per bin and sort the output

---
+++ sandbox/ncbi/split_dbsnp_bin.py
@@ -0,0 +1,42 @@
+import sys
+import json
+import os
+
+
+def make_bin_key(var_key):
+    chrom, pos = var_key.split(':')[:2]
+    bin_key = '{}.{}'.format(chrom, pos[:3])
+    return bin_key
+
+
+def sort_json(json_name):
+
+    print('sorting ' + json_name, file=sys.stderr)
+    sort_cmd = 'sort -t\: -gk2,2 {} -o {}'.format(json_name, json_name)
+    print('> ' + sort_cmd, file=sys.stderr)
+    os.system(sort_cmd)
+
+
+def split_dbsnp_bin(json_name, out_dir):
+
+    fout_dict = {}
+
+    print('reading ' + json_name, file=sys.stderr)
+    for line in open(json_name):
+        line = line.rstrip()
+        var_key, value = line.split('\t')
+        bin_key = make_bin_key(var_key)
+        out_json_name = '{}/{}.dbsnp.json'.format(out_dir, bin_key)
+        if out_json_name not in fout_dict:
+            fout_dict[out_json_name] = open(out_json_name, 'w')
+        print(line, file=fout_dict[out_json_name])
+
+    for out_json_name in fout_dict:
+        fout_dict[out_json_name].close()
+        sort_json(out_json_name)
+
+
+if __name__ == '__main__':
+
+    json_name, out_dir = sys.argv[1:3]
+    split_dbsnp_bin(json_name, out_dir)
